8521353f22b7c71a2914590d5f3e66856cf0d6f7
Avej <avejs2272@gmail.com>
2020-08-15 01:24:13 +0530

    Saved Changes

---
+++ TBA Project/Suitelet.js
@@ -0,0 +1,30 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType Suitelet
+ * @NModuleScope SameAccount
+ */
+define(['N/record','N/log', 'N/util'],
+/**
+ * @param {record} record
+ */
+function(record,log,util) {
+   
+    /**
+     * Definition of the Suitelet script trigger point.
+     *
+     * @param {Object} context
+     * @param {ServerRequest} context.request - Encapsulation of the incoming request
+     * @param {ServerResponse} context.response - Encapsulation of the Suitelet response
+     * @Since 2015.2
+     */
+    function onRequest(context) {
+                
+  
+    
+    }
+
+    return {
+        onRequest: onRequest
+    };
+    
+});
